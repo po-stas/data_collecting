3103455a5c6f1cf94dba5c6474998e026c0052c7
postas <pologrudov@gmail.com>
2019-11-28 22:13:21 -0800

    Lesson 2 sj parser almost done

--- Lesson2.py
+++ Lesson2.py
@@ -15,7 +15,7 @@ import re
 import pandas as pd
 
 
-def parse_hh_compensation(comp):
+def parse_compensation(comp):
     salary_from = salary_to = units = ''
     comp_search = re.search('(от)?([0-9 ]+)?(до|-)?([0-9 ]+)? (.*)$', comp.replace('\xa0', ''))
     if comp_search:
@@ -23,7 +23,7 @@ def parse_hh_compensation(comp):
             salary_from = int(comp_search.group(2))
         if comp_search.group(3) and '-' in comp_search.group(3) or comp_search.group(3) and 'до' in comp_search.group(3):
             salary_to = int(comp_search.group(4))
-        elif 'до' in comp_search.group(1):
+        elif comp_search.group(1) and 'до' in comp_search.group(1):
             salary_to = int(comp_search.group(2))
 
         units = comp_search.group(5)
@@ -54,7 +54,7 @@ def get_from_hh(link):
                 if comp:
                     comp_text = comp.getText()
 
-                salary_from, salary_to, units = parse_hh_compensation(comp_text)
+                salary_from, salary_to, units = parse_compensation(comp_text)
                 vacancy_data.extend((salary_from, salary_to, units))
 
                 vacancy_data.append('hh.ru')
@@ -66,12 +66,38 @@ def get_from_hh(link):
     return vacancies_data
 
 
-data = get_from_hh('https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=python')
+def get_from_sj(link):
 
-if data:
-    df = pd.DataFrame(data, columns=['Вакансия', 'Ссылка', 'ЗП от', 'ЗП до', 'валюта', 'Источник'])
-    pd.set_option('display.max_columns', 6)
-    pd.set_option('display.width', 1000)
-    print(df)
-else:
-    print('Ничего не найдено...')
+    with open('SuperJob2.html', 'r') as file:
+        html = file.read()
+
+    parsed_html = bs(html, 'lxml')
+    vacancies = parsed_html.find_all('div', {'class': '_3zucV _2GPIV f-test-vacancy-item i6-sc _3VcZr'})
+
+    vacancies_data = []
+    for vacancy in vacancies:
+        vacancy_data = []
+        a = vacancy.findChildren('a', {'class': '_1QIBo'})
+        link = a[0]['href']
+        title_block = a[0].findChildren('div', {'class': '_3mfro'})
+        title = ''.join(title_block[0].findAll(text=True, recursive=True))
+        print(title)
+        salary_block = vacancy.findChildren('span', {'class': '_2Wp8I'})
+        if salary_block:
+            salary = salary_block[0].getText().replace('\xa0—\xa0', '-')
+            salary = re.sub(r'(\d+)\xa0(\d+)', r'\1\2', salary).replace('\xa0', ' ')
+            salary_from, salary_to, units = parse_compensation(salary)
+        pass
+
+
+get_from_sj('')
+# data = get_from_hh('https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=python')
+
+
+# if data:
+#     df = pd.DataFrame(data, columns=['Вакансия', 'Ссылка', 'ЗП от', 'ЗП до', 'валюта', 'Источник'])
+#     pd.set_option('display.max_columns', 6)
+#     pd.set_option('display.width', 1000)
+#     print(df)
+# else:
+#     print('Ничего не найдено...')
